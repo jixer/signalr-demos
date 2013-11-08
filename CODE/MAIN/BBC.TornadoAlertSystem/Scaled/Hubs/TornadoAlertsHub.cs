ad2c793248bc0733ff4719d548fcd2d2078d8aa9
jixer <me@bloggedbychris.com>
2013-11-08 00:46:32 -0800

    Copied basic example to scaled solution area

---
+++ CODE/MAIN/BBC.TornadoAlertSystem/Scaled/Hubs/TornadoAlertsHub.cs
@@ -0,0 +1,17 @@
+﻿using BBC.TornadoAlertSystem.Web.Models;
+using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BBC.TornadoAlertSystem.Web.Hubs
+{
+    public class TornadoAlertsHub : Hub
+    {
+        public void SendTornadoAlert(TornadoAlert alert)
+        {
+            Clients.All.addTornadoAlertMessage(alert);
+        }
+    }
+}
---
+++ CODE/MAIN/BBC.TornadoAlertSystem/Scaled/Scripts/basic-alerts.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="jquery-1.8.2.intellisense.js" />
+$(function () {
+    var tornadoHubProxy = $.connection.tornadoAlertsHub;
+    tornadoHubProxy.client.addTornadoAlertMessage = function (alertMessage) {
+        var tornadoAlertDiv = $("<div><h3>New Alert!!</h3>Name: " + alertMessage.Name + "<br />Location: " + alertMessage.Location + "<br /></div>");
+        $("#tornado-warnings").append(tornadoAlertDiv);
+    };
+
+    $.connection.hub.start()
+        .done(function () { console.log('Now connected, connection ID=' + $.connection.hub.id); })
+        .fail(function () { console.log('Could not Connect!'); });
+
+    $("#submit-alert").click(function () {
+        var tornadoAlert = {
+            Name: $("#name").val(),
+            Location: $("#location").val()
+        };
+        tornadoHubProxy.server.sendTornadoAlert(tornadoAlert);
+    });
+});
