ee9c00b10b092c87844024643a9d367bcfa808be
jixer <me@bloggedbychris.com>
2013-11-08 02:01:44 -0800

    Configured SignalR and tested solution

--- CODE/MAIN/BBC.TornadoAlertSystem/Scaled/Startup.cs
+++ CODE/MAIN/BBC.TornadoAlertSystem/Scaled/Startup.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
 using Owin;
 
 namespace BBC.TornadoAlertSystem.Web
 {
     public class Startup
-    {
+    {        
         public void Configuration(IAppBuilder app)
         {
+            string sbConnectionString = "Endpoint=sb://sigr-demo.servicebus.windows.net;SharedSecretIssuer=owner;SharedSecretValue=cFp+1HEc7OyhqcbrmVME3J/NdWY5tyUNIDtkft6KRUY=";
+            string sbTopicPrefix = "backplane";
+            GlobalHost.DependencyResolver.UseServiceBus(sbConnectionString, "backplane");
+
             app.MapSignalR();
         }
     }
