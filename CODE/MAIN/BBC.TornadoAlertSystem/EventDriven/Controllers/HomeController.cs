3108f9493b6a19432267b28b0ab4873be9b3e61f
jixer <me@bloggedbychris.com>
2013-11-08 02:06:05 -0800

    Copied Scale solution to event driven solution

---
+++ CODE/MAIN/BBC.TornadoAlertSystem/EventDriven/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BBC.TornadoAlertSystem.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Enter or monitor tornado alerts.";
+
+            return View();
+        }
+    }
+}
